d9c426a59e049c533eedeee5756f91ed1d194da5
chaeyoun <chaeyounsong@gmail.com>
2019-10-10 20:33:46 +0900

    added routes
    added routes

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req,res){
+    console.log('req');
+    res.send('hello. From home.');
+}
+
+
+function handleProfile(req,res){
+
+    console.log('req');
+    res.send('You are on my Profile.')
+}
+
+app.get("/profile",handleProfile);
+app.get("/",handleHome);
+
 app.listen(PORT, handleListening);
