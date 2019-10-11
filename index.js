9005434afc97081a65d4fe5adb32b3f385c77cb4
chaeyoun <chaeyounsong@gmail.com>
2019-10-11 20:25:40 +0900

    added middlewear format
    added middlewear format.

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const handleListening = () =>
 console.log(`Listening on : http://localhost:${PORT}`)
 
 const handleHome = (req,res) => res.send('Hello! From home.');
-const handleProfile = (req, res) => res.send('You are on my profile!');
+const handleProfile = (req, res) => res.send('You are on my profile.');
 
-app.get("/profile",handleProfile);
+const betweenHome = (req,res,next) => {
+    console.log('Between');
+    next();
+}
+
+app.use(betweenHome);
 app.get("/",handleHome);
+app.get("/profile",handleProfile);
+
 
 app.listen(PORT, handleListening);
