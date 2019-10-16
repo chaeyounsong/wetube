80e64abc89c31dc03cc2fd56505fb9581d212a11
chaeyoun <chaeyounsong@gmail.com>
2019-10-16 15:00:50 +0900

    Created controller
    usercontrollers > join, login, logout
    videocontorollers > search,home

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,7 +3,7 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req,res) => res.send('Users'));
+userRouter.get('/', (req,res) => res.send('Users'));
 userRouter.get(routes.userDetail, (req,res) => res.send('User Datail'));
 userRouter.get(routes.editProfile, (req,res) => res.send('Edit Profile'));
 userRouter.get(routes.changePassword, (req,res) => res.send('Change Password'));
