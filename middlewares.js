3fb20ff99c3ebb75479a6b58999208a8d64c62c9
chaeyoun <chaeyounsong@gmail.com>
2019-10-17 23:05:50 +0900

    Created videocontroller
    (1) added column:form action=method="get"
    and input box (named term)
    from header.pug
    
    (2) added function videoController
    const search = (req,res) =>{
         const{query:{term:searchingBy}}=req;
    }
    
    get ="value" > const searchingBy = req.query.term
    
    (etc : error serch.pug > search.pug)

--- middlewares.js
+++ middlewares.js
@@ -2,6 +2,7 @@ import routes from "./routes";
 
 export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = "WeTube";
+    res.locals.pageTitle = ""
     res.locals.routes = routes;
     next();
 };
