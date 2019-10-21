823a84b4d2e3b76f04e10c904ca171dca5817706
chaeyoun <chaeyounsong@gmail.com>
2019-10-21 16:00:17 +0900

    added join controller
    res :
    joinGet,
    joinPost
    
    globalRouter :
    routes.join, getJoin
    join, pageTitle : join
    
    postjoin : body(name,email,password,password2(verify if func)) setting true or false
    true : res.redifrect(routes.home)
    false : res.status(400)
    
    with
    bodyParser
    (can read body info written by user)

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,22 @@
+import routes from "../routes";
+
 export const users = (req,res) => res.render('users', { pageTitle: "Users"});
-export const join = (req,res) => res.render('join', { pageTitle: "Join"});
+export const getJoin = (req,res) => res.render('join', { pageTitle: "Join"});
+
+export const postJoin = (req,res) => {
+    const {
+        body : {name,email,password,password2}
+    } = req;
+    if(password !== password2){
+        res.status(400);
+        res.render("join", {pageTitle: "Join"});
+    } else{
+        //To Do : Register User
+        //To Do : Log user in
+        res.redirect(routes.home)
+    }
+};
+
 export const login = (req,res) => res.render('login', { pageTitle: "Log-in"});
 export const logout = (req,res) => res.render('logout', { pageTitle: "Log-out"});
 export const changePassword = (req,res) => res.render('changePassword', { pageTitle: "Change Password"});
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,5 +1,4 @@
 import { videos } from "../db";
-
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -8,7 +7,7 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) =>
