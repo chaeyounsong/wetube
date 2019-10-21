35ce0739b7559e699bd7fe45ca9894b6071bb791
chaeyoun <chaeyounsong@gmail.com>
2019-10-21 18:26:26 +0900

    Creadted login, Temporary ID DB
    login : !user.inAuthenicated
    > join, log-in
    
    else : upload, profile, logout
    
    middlewares : make TempoID
    res.locals.user={
    isAutenticated : true,
    id :1
    }
    
    userDetail : (id)=>{
    }

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,10 @@
 import routes from "../routes";
 
 export const users = (req,res) => res.render('users', { pageTitle: "Users"});
-export const getJoin = (req,res) => res.render('join', { pageTitle: "Join"});
 
+
+// Join
+export const getJoin = (req,res) => res.render('join', { pageTitle: "Join"});
 export const postJoin = (req,res) => {
     const {
         body : {name,email,password,password2}
@@ -17,7 +19,13 @@ export const postJoin = (req,res) => {
     }
 };
 
-export const login = (req,res) => res.render('login', { pageTitle: "Log-in"});
+// Log-in
+export const getlogin = (req,res) => res.render('login', { pageTitle: "Log-in"});
+export const postLogin = (req,res) => {
+    res.redirect(routes.home);
+}
+
+
 export const logout = (req,res) => res.render('logout', { pageTitle: "Log-out"});
 export const changePassword = (req,res) => res.render('changePassword', { pageTitle: "Change Password"});
 export const editProfile = (req,res) => res.render('editProfile', { pageTitle: "Edit Profile"});
